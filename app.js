e6769be5dbe1db211e1d0bc30f66767c5e7bc1eb
Albi <albikrasniqi31@gmail.com>
2023-06-15 02:29:29 +0200

    add public and views file

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Ejs template engine
+app.set("view engine", "ejs");
+
+// Static files middleware
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Index Sayfasi");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
