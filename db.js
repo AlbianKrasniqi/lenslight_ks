750e308927ee1ec5ca1f17ad0eccaa0d7245ec19
Albi <albikrasniqi31@gmail.com>
2023-06-15 22:12:02 +0200

    connection to the DB

---
+++ db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const conn = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to the DB succesfully");
+    })
+    .catch((err) => {
+      console.log(`DB connection err:, ${err}`);
+    });
+};
+
+export default conn;
