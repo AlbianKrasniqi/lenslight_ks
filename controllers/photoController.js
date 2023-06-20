d164bd5bbbc147bcc472c7d9da5c21ee94a51b74
Albi <albikrasniqi31@gmail.com>
2023-06-20 21:57:24 +0200

    single photo route

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -30,4 +30,19 @@ const getAllPhotos = async (req, res) => {
   }
 };
 
-export { createPhoto, getAllPhotos };
+const getPhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById({ _id: req.params.id });
+    res.status(201).render("photo", {
+      photo,
+      link: "photos",
+    });
+  } catch (err) {
+    res.status(500).json({
+      succeed: false,
+      err,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos, getPhoto };
