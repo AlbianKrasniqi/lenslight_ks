60bfd3bfa80e13a513e1380acf7830cc30e15564
Albi <albikrasniqi31@gmail.com>
2023-06-23 01:37:39 +0200

    login, bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 const createUser = async (req, res) => {
   try {
@@ -15,4 +16,37 @@ const createUser = async (req, res) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username: username });
+
+    let same = false;
+
+    if (user) {
+      same = await bcrypt.compare(password, user.password);
+    } else {
+      return res.status(401).json({
+        succeed: false,
+        err: "There is no such user",
+      });
+    }
+
+    if (same) {
+      res.status(200).send("You are loggend in");
+    } else {
+      res.status(401).json({
+        succeded: false,
+        err: "Password are not matched",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      succeed: false,
+      err,
+    });
+  }
+};
+
+export { createUser, loginUser };
